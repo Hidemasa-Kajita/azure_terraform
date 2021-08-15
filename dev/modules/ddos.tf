d1817d8261c9f3fecc623fa471d998ba7970137c
梶田英完 <hidemasa_kajita@respawn.jp>
2021-08-15 18:07:16 +0900

    comment out ddos plan

--- dev/modules/ddos.tf
+++ dev/modules/ddos.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_ddos_protection_plan" "ddos" {
+/* resource "azurerm_network_ddos_protection_plan" "ddos" {
   name                = "${var.service_name}-ddos"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -6,4 +6,4 @@ resource "azurerm_network_ddos_protection_plan" "ddos" {
   tags = {
     environment = var.env
   }
-}
+} */
--- dev/modules/vnet.tf
+++ dev/modules/vnet.tf
@@ -6,10 +6,10 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   # dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  ddos_protection_plan {
+  /* ddos_protection_plan {
     id     = azurerm_network_ddos_protection_plan.ddos.id
     enable = true
-  }
+  } */
 
   tags = {
     environment = var.env
